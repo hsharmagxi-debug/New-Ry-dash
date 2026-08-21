b0333400163e58141a45a6938e692db9ecd0750f
Urvashi Chandan <ashiurvi786@gmail.com>
2026-08-21 21:43:16 +0530

    Remove accidentally re-committed Vite cache folder, gitignore .vite again (the new zip's own .gitignore didn't have it)

--- .vite/deps/three_examples_jsm_postprocessing_ShaderPass__js.js
+++
@@ -1,9 +0,0 @@
-import {
-  ShaderPass
-} from "./chunk-6QDNAWVD.js";
-import "./chunk-3CK76CR2.js";
-import "./chunk-Z7OYAWFU.js";
-export {
-  ShaderPass
-};
-//# sourceMappingURL=three_examples_jsm_postprocessing_ShaderPass__js.js.map
